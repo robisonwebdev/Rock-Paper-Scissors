d54d11a3bd4be36ab8232d78069a5cacf11300e1
David Robison <david@robisonweb.dev>
2021-01-25 17:51:42 -0700

    create variable called randomGamePiece to randomly pick a number between 0 and 3

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function computerPlay() {
     let gamePieces = ['Rock', 'Paper', 'Scissors'];
+    let randomGamePiece = Math.floor(Math.random() * 4);
+
+    return gamePieces[randomGamePiece];
 }
 
 function playSingleRound(playerSelections, computerSelection) {
