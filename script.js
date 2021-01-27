56491d64bdf31a51c218f0ff0677e479e897b915
David Robison <david@robisonweb.dev>
2021-01-26 20:34:59 -0700

    Tested playSingleRound to checked if param correct, they are correct

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 // Get player input
-    // Display a prompt asking user to pick between rock, paper, or scissors
-    // Save selection in variable
-
-// Get computer input
-    // Have computer pick randomly between rock, paper, or scissors
-    // Save selection in variable
-
-// Compare player input to computer input
-    // Rock beats scissors
-    // Scissors beats paper
-    // Paper beats rock
-
-// Display winner
-// Track score
-// Reset
-
-function computerPlay() {
-    const gamePieces = ['Rock', 'Paper', 'Scissors'];
-    const random = Math.floor(Math.random() * 3);
-
-    return gamePieces[random];
-}
+// Display a prompt asking user to pick between rock, paper, or scissors
+// Save selection in variable
 
 function playerPlay() {
     return prompt('Select Rock, Paper, or Scissors');
 }
 
-function playSingleRound(playerSelections, computerSelection) {
+// Get computer input
+// Have computer pick randomly between rock, paper, or scissors
+// Save selection in variable
+
+function computerPlay() {
+    const gamePieces = ['Rock', 'Paper', 'Scissors'];
+    const random = Math.floor(Math.random() * 3);
     
+    return gamePieces[random];
 }
+
+// Compare player input to computer input
+// Rock beats scissors
+// Scissors beats paper
+// Paper beats rock
+function playSingleRound(playerSelections, computerSelection) {
+    console.log(playerSelections);
+    console.log(computerSelection)
+}
+
+// Display winner
+// Track score
+// Reset
+
+
+playSingleRound(playerPlay(), computerPlay());
