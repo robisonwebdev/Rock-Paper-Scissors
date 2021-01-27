93992ac07c08aa3d4ec2df867258bc0b062c5b0f
David Robison <david@robisonweb.dev>
2021-01-26 19:02:07 -0700

    Pseudocode

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Get player input
+    // Display a prompt asking user to pick between rock, paper, or scissors
+    // Save selection in variable
+
+// Get computer input
+    // Have computer pick randomly between rock, paper, or scissors
+    // Save selection in variable
+
+// Compare player input to computer input
+    // Rock beats scissors
+    // Scissors beats paper
+    // Paper beats rock
+
+// Display winner
+// Track score
+// Reset
+
 function computerPlay() {
     const gamePieces = ['Rock', 'Paper', 'Scissors'];
     const random = Math.floor(Math.random() * 3);
